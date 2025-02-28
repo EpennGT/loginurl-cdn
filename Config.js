790aaf953f362981795cfa1b30078429a6c55a5e
EpennGT <razerboy518@gmail.com>
2025-02-28 19:57:49 +0800

    Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'GrowPlus',   // name of the server
+    name: 'Tamtopia',   // name of the server
 
     server_data: {
-        ip: "127.0.0.1",    // your enet host
+        ip: "159.65.140.105",    // your enet host
         port: "17091",  // your enet port
         meta: "localhost",
-        loginurl: "137.0.0.1"   // login dashboard
+        loginurl: ""   // login dashboard
     }
 };
